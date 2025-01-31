3a4eb53d36e9ba66a38c028dfd24434c1b970f75
Jude Agboola <marvinjudehk@gmail.com>
2025-01-31 16:21:59 +0100

    use content type when uploading

--- src/lib/s3-utils.ts
+++ src/lib/s3-utils.ts
@@ -20,28 +20,29 @@ const s3Client = new S3Client({
  * @returns Promise<string> - The S3 URL where the file was uploaded
  */
 export async function processAndUploadFile(downloadURI: string, s3Key: string) {
-  const { buffer, extension } = await downloadFile(downloadURI);
-  
-  return await uploadToS3(s3Key, buffer, extension ?? "");
+  const { buffer, extension, contentType } = await downloadFile(downloadURI);
 
+  return await uploadToS3(s3Key, buffer, extension ?? "", contentType);
 }
 
 /**
  * Downloads a file from a given URI and uploads it to S3
  * @param downloadURI - The URI to download the file from
- * @param s3Key - The key (path) where the file will be stored in S3
- * @returns Promise<string> - The S3 URL where the file was uploaded
+ * @param s3Key - The key (path) where the file will be stored xin S3
+ * @returns Promise<string> - The S3 URL where the fxile was uploaded
  */
 export async function uploadToS3(
   key: string,
   body: Buffer,
-  extension: string
+  extension: string,
+  contentType: string
 ): Promise<string> {
   const keyWithExtension = `${key}.${extension}`;
   const uploadParams = {
     Bucket: process.env.AWS_BUCKET_NAME,
     Key: keyWithExtension,
     Body: body,
+    ContentType: contentType,
   };
 
   await s3Client.send(new PutObjectCommand(uploadParams));
