8315690f6ee24871c896bbbd2369373053284d9f
Jude Agboola <marvinjudehk@gmail.com>
2025-02-04 15:02:23 +0100

    add update route

--- src/app/api/webhooks/on-update/route.ts
+++ src/app/api/webhooks/on-update/route.ts
@@ -1,22 +1,53 @@
+import { DocumentModel } from "@/models/document";
 import { NextResponse } from "next/server";
-// import { z } from "zod";
+import { z } from "zod";
 
-
-// const WebhookPayloadSchema = z.object({
-//   source: z.string(),
-//   id: z.string(),
-// });
+const webhookSchema = z.object({
+  connectionId: z.string(),
+  fields: z.object({
+    id: z.string().min(1),
+    title: z.string().min(1),
+    createdAt: z.string().datetime(),
+    updatedAt: z.string().datetime(),
+    parentId: z.string().min(1),
+    canHaveChildren: z.boolean(),
+    resourceURI: z.string().url(),
+  }),
+});
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
 
+    console.log("body", body);
 
-    console.log("Webhook received:", body);
+    const payload = webhookSchema.safeParse(body);
+
+    if (!payload.success) {
+      console.error("Invalid webhook payload:", payload.error);
+      return NextResponse.json(
+        { error: "Invalid webhook payload" },
+        { status: 400 }
+      );
+    }
+
+    const { fields, connectionId } = payload.data;
+
+    // TODO: Spread fields once we fix issue in drive connector
+    await DocumentModel.updateOne(
+      { id: fields.id, connectionId },
+      {
+        $set: {
+          title: fields.title,
+          updatedAt: fields.updatedAt,
+          resourceURI: fields.resourceURI,
+        },
+      }
+    );
 
     return NextResponse.json({ message: "ok" });
   } catch (error) {
-    console.error("Error in on-delete webhook:", error);
+    console.error("Error in on-update webhook:", error);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
