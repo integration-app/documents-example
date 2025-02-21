d622195b9cecb48e90e4a64f9bfe9ae990f490c0
Jude Agboola <marvinjudehk@gmail.com>
2025-02-21 18:18:18 +0100

    extract text fron the JSON

--- src/app/api/webhooks/on-download-complete/text-extraction-utils.ts
+++ src/app/api/webhooks/on-download-complete/text-extraction-utils.ts
@@ -32,7 +32,7 @@ export const isSupportedFile = (name: string) => {
 };
 
 export const convertJSONToText = (elements: PartitionResponse["elements"]) => {
-  return JSON.stringify(elements);
+   elements.map(element => element.text).join('\n\n');
 };
 
 const extractTextFromFile = async ({
