feadec0096ca98cbee9f80623e2dbed7e0b24e4e
Jude Agboola <marvinjudehk@gmail.com>
2025-03-13 17:44:37 +0100

    increase limit on inngest endpoint

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,6 +2,8 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { inngest_downloadAndExtractTextFromFile } from "../webhooks/on-download-complete/downloadAndExtractTextFromFile";
 
+export const maxDuration = 60;
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [inngest_downloadAndExtractTextFromFile],
