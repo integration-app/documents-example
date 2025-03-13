b99e76338af1572f2d0742b95b37e2d4ca66e171
Jude Agboola <marvinjudehk@gmail.com>
2025-03-13 17:33:15 +0100

    add vercel.json to fix step timeout

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,8 +2,6 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { inngest_downloadAndExtractTextFromFile } from "../webhooks/on-download-complete/downloadAndExtractTextFromFile";
 
-export const maxDuration = 60;
-
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [inngest_downloadAndExtractTextFromFile],
